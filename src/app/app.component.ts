adbf536a343b552c6c2c0f7f5c52e0643763d672
Filiberto <filiberto-gonzalezj@unilibre.edu.co>
2019-08-26 20:15:10 -0500

    comit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'app';
+  var1 = 10;
+  var2 = 20;
+  deshabilitado = true;
+
+  nombre:string = '';
+paises:any = [
+  {activo:true, nombre: 'Colombia'},
+  {activo:true, nombre: 'Brasil'},
+  {activo:false, nombre: 'Argentina'},
+  {activo: true, nombre:  'Polonia'},
+  {activo: false, nombre: 'Alemania'},
+  {activo: true, nombre: 'Francia'}
+]
+
+constructor(){
+  setTimeout(() => {
+    this.deshabilitado = false;
+  }, 3000)
+}
+ejecutar(){
+  alert("Ejecutando...")
+}
+
+
+
+
+
 }
